6dbcc175fc2018b07a3260e259e0f2e99dbce254
nasseralbess <nasseralbess98@gmail.com>
2026-03-30 10:06:29 +0300

    minor changes

--- extract_text_activations.py
+++ extract_text_activations.py
@@ -3,10 +3,12 @@ from transformers import AutoProcessor
 from dictionary_learning import utils
 from transformers import Gemma3ForConditionalGeneration
 import json
+from tqdm import tqdm
 TEXT_DIR = "text_dataset"
 device = "cuda:0"
 model_id = "google/gemma-3-27b-it"
 model = Gemma3ForConditionalGeneration.from_pretrained(model_id, device_map="cuda:0")
+model.eval()
 processor = AutoProcessor.from_pretrained(model_id)
 
 layers_of_interest = [10,30,59]
@@ -21,14 +23,15 @@ for layer in layers_of_interest:
 
 
 
-def prepare_text_activation(prompts, classes, layers_of_interest=[10,30,50], batch_size=32, top_k=20):
+def prepare_text_activation(prompts, classes, layers_of_interest=[10,30,50], batch_size=32, top_k=20, concept=True):
     top_activation_dict = {layer:{"top_values":[],"top_indices":[]} for layer in layers_of_interest}
     layer_SAEs = {}
     for layer in layers_of_interest:
         trained_sae, _ = utils.load_dictionary(f"activations_{layer}/trainer_0", device="cuda:0")
+        trained_sae.eval()
         layer_SAEs[layer] = trained_sae
 
-    for i in range(0,len(prompts),batch_size):
+    for i in tqdm(range(0,len(prompts),batch_size), desc=f"prompts with batch size {batch_size}"):
         if i+batch_size+1<len(prompts):
             messages = [[
                 {
@@ -45,7 +48,7 @@ def prepare_text_activation(prompts, classes, layers_of_interest=[10,30,50], bat
             inputs = processor.apply_chat_template(
                 messages, add_generation_prompt=True, tokenize=True,
                 return_dict=True, return_tensors="pt", padding=True
-            )
+            ).to(model.device, dtype=torch.bfloat16)
             tokenized = [[processor.decode(id) for id in inputs["input_ids"][b]] for b in range(len(inputs["input_ids"]))]
         else:
             messages = [[
@@ -69,19 +72,23 @@ def prepare_text_activation(prompts, classes, layers_of_interest=[10,30,50], bat
         activations.clear()
         with torch.inference_mode():
             model(**inputs)
-        
-        concept_token_indices = [j for k in range(len(tokenized)) for j,token in enumerate(tokenized[k]) if token in classes[f"{i+k}"]["labels"] and classes[f"{i+k}"]["labels"][token]=="1"]
+        if not concept:
+            concept_token_indices = [j for k in range(len(tokenized)) for j,token in enumerate(tokenized[k]) if token not in classes[f"{i+k}"]["labels"] or classes[f"{i+k}"]["labels"][token]=="0"]
+        else:
+            concept_token_indices = [j for k in range(len(tokenized)) for j,token in enumerate(tokenized[k]) if token in classes[f"{i+k}"]["labels"] and classes[f"{i+k}"]["labels"][token]=="1"]
+            
         for layer in layers_of_interest:
-            feats = layer_SAEs[layer](activations[layer], output_features=True)[1]
-            feats.flatten(end_dim=1)[concept_token_indices]
-            top_feature_values, top_feature_indices = feats.abs().sum(dim=0).topk(top_k)
-            top_activation_dict[layer]["top_values"].append(top_feature_values)
-            top_activation_dict[layer]["top_indices"].append(top_feature_indices)
+            with torch.no_grad():
+                feats = layer_SAEs[layer](activations[layer].to(dtype=torch.float32), output_features=True)[1]
+                feats = feats.flatten(end_dim=1)[concept_token_indices]
+                top_feature_values, top_feature_indices = feats.abs().sum(dim=0).topk(top_k)
+                top_activation_dict[layer]["top_values"].append(top_feature_values.detach().float().cpu().tolist())
+                top_activation_dict[layer]["top_indices"].append(top_feature_indices.detach().float().cpu().tolist())
     return top_activation_dict
 
 if __name__=="__main__":
     prompts = json.load(open(f"{TEXT_DIR}/text_concept_a_person.json"))
     classes = json.load(open(f"{TEXT_DIR}_classified/text_concept_a_person_classified.json"))
-    top_activations_dict = prepare_text_activation(prompts=prompts, classes=classes, layers_of_interest=layers_of_interest)
+    top_activations_dict = prepare_text_activation(prompts=prompts, classes=classes, layers_of_interest=layers_of_interest, batch_size=2)
     with open("text_feature_discovery.json","w") as f:
         json.dump(top_activations_dict, f)
