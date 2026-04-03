9f52bc7dc3baf933ac577a5e38135528b6465482
KindaMashal <kindamashaal@gmail.com>
2026-04-04 00:15:03 +0300

    fixed dataset filtering-coco

--- image_set_curation.py
+++ image_set_curation.py
@@ -3,6 +3,7 @@ import random
 import os
 import shutil
 import urllib.request, zipfile
+from ultralytics import YOLO
 
 
 target_concept1 = "person"
@@ -24,6 +25,7 @@ annotated_file= r"C:\Users\kinda\Documents\.1Psut\season finale\GP\dataset curat
 image_directory= r"C:\Users\kinda\Documents\.1Psut\season finale\GP\dataset curation\val2017\val2017"
 output_directory= "clean_dataset"
 coco = COCO(annotated_file)
+model = YOLO("yolo26n.pt")
 
 
 def folder_dataset_creation(name):
@@ -44,7 +46,37 @@ def copy_images(img_ids, target_folder):
 
 
 
-def getting_both_concept_images(first_concept, second_concept,size=30):
+def visible_concepts(image,concept,box_size):
+
+    results = model(image)
+    
+    area_concept=[]
+    for result in results:
+        boxes = result.boxes 
+
+        for box in boxes:
+
+            class_id = int(box.cls[0])
+            class_name = model.names[class_id]
+            
+            if class_name == concept:
+                
+                x1, y1, x2, y2 = box.xyxy[0]
+                
+                width = x2 - x1
+                height = y2 - y1
+                area= width*height
+                area_concept.append(area)
+ 
+    
+    for area in area_concept:
+        if area>box_size:
+            return True
+
+
+
+
+def getting_both_concept_images(first_concept, second_concept,size=30,diversity=True):
 
     concept1_cat_id = coco.getCatIds(catNms=[first_concept])
     concept2_cat_id = coco.getCatIds(catNms=[second_concept])
@@ -53,15 +85,27 @@ def getting_both_concept_images(first_concept, second_concept,size=30):
     ids2 = set(coco.getImgIds(catIds=concept2_cat_id))
 
     concept_img_ids = list(ids1 & ids2)
-    print(f"total of both {first_concept} & {second_concept} concepts images found: ", len(concept_img_ids))
+    #print(f"total of both {first_concept} & {second_concept} concepts images found: ", len(concept_img_ids))
+    if not diversity:
+        image_ids= random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+        final_list=[]
 
-    return random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+        for id in image_ids:
+            img = coco.loadImgs(id)[0]
+            src = os.path.join(image_directory, img["file_name"])
+            area1=visible_concepts(src,first_concept,10000)
+            area2=visible_concepts(src,second_concept,10000)
+            if area1 and area2:
+                final_list.append(id)
+        
+        return final_list
+    else:
+        return random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+    
 
 
 
-
-
-def getting_one_concept_images(main_concept, remove_concept, both_present=False,concept_present="",size=30):
+def getting_one_concept_images(main_concept, remove_concept,size=30, diversity=False):
 
     main_cat_id = coco.getCatIds(catNms=[main_concept])
     removed_cat_id = coco.getCatIds(catNms=[remove_concept])
@@ -70,16 +114,29 @@ def getting_one_concept_images(main_concept, remove_concept, both_present=False,
     removed_ids = set(coco.getImgIds(catIds=removed_cat_id))
 
     concept_img_ids = list(main_ids - removed_ids)
-    print(f"total of the {main_concept} concept images found: ", len(concept_img_ids))
+    #print(f"total of the {main_concept} concept images found: ", len(concept_img_ids))
+    if not diversity:
+        image_ids= random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+        final_list=[]
 
-    return random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+        for id in image_ids:
+            img = coco.loadImgs(id)[0]
+            src = os.path.join(image_directory, img["file_name"])
+            area=visible_concepts(src,main_concept,30000)
+            if area:
+                final_list.append(id)
+        
+        return final_list
+    else:
+        return random.sample(concept_img_ids, min(size, len(concept_img_ids)))
+
 
 
 
 def non_concept_images(concept, size):
     all_imgs = set(coco.getImgIds())
     category_ids = coco.getCatIds(catNms=[concept])
-    concept_img_ids = coco.getImgIds(catIds=[category_ids])
+    concept_img_ids = coco.getImgIds(catIds=category_ids)
     non_concept_imgs = list(all_imgs - set(concept_img_ids))
     print("total of the non concept images: ", len(non_concept_imgs))
     return random.sample(non_concept_imgs, size)
@@ -89,9 +146,9 @@ def non_concept_images(concept, size):
 
 
 
-# copy_images(getting_concept_images(target_concept1,target_concept2,size=target_size), folder_dataset_creation(target_concept1))
-# copy_images(getting_concept_images(target_concept2,target_concept1,size=target_size), folder_dataset_creation(target_concept2))
-# copy_images(getting_concept_images(target_concept1,target_concept2,both_present=True,size=target_size), folder_dataset_creation(target_concept1+'_&_'+target_concept2))
+copy_images(getting_one_concept_images(target_concept1,target_concept2,size=target_size), folder_dataset_creation(target_concept1))
+copy_images(getting_one_concept_images(target_concept2,target_concept1,size=target_size), folder_dataset_creation(target_concept2))
+copy_images(getting_both_concept_images(target_concept1,target_concept2,size=target_size,diversity=False), folder_dataset_creation(target_concept1+'_&_'+target_concept2))
 
 
 
