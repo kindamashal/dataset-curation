e43a86d78689d011687440e453235c161712affd
nasseralbess <nasseralbess98@gmail.com>
2026-04-19 12:07:19 +0300

    Scripts for feature extraction from activations

---
+++ src/features/extract/print_text_stats.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.append(".")
+from utils import full_feature_set, calculate_precision, freq_dicts, top20_stats
+import json
+import argparse
+
+import json
+import argparse
+
+concept_path = "activations/text/male_concept_direct_prompt.json"
+non_concept_path = "activations/text/male_non_concept_direct_prompt.json"
+layers_of_interest = [10, 30, 59]
+
+
+def specificity_feats(layer, concept_dict, non_concept_dict):
+    all_feats = full_feature_set(concept_dict, non_concept_dict, layer)
+    precision = {}
+    for feat in all_feats:
+        precision[feat] = calculate_precision(
+            feat, concept_dict, non_concept_dict, layer
+        )
+    freq, freq2 = freq_dicts(concept_dict, non_concept_dict, layer)
+    l = sorted(
+        [(int(i), freq[i], precision[i]) for i in precision if i in freq],
+        key=lambda x: (x[2], x[1]),
+        reverse=True,
+    )
+    print(l)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Print activation feature stats")
+    parser.add_argument(
+        "--concept-path", dest="concept_path", type=str, default=concept_path
+    )
+    parser.add_argument(
+        "--non-concept-path",
+        dest="non_concept_path",
+        type=str,
+        default=non_concept_path,
+    )
+    parser.add_argument(
+        "--layers", dest="layers", type=int, nargs="+", default=layers_of_interest
+    )
+
+    args = parser.parse_args()
+    concept_path = args.concept_path
+    non_concept_path = args.non_concept_path
+    layers_of_interest = args.layers
+
+    outs = json.load(open(concept_path))
+    outs2 = json.load(open(non_concept_path))
+    for layer in layers_of_interest:
+        print("Top 20 stats for layer",layer)
+        print("="*100)
+        print(top20_stats(outs, outs2, layer, return_feats=False))
+        print("\nPrecision + count stats for all the features:")
+        print("="*100)
+        specificity_feats(layer, outs, outs2)
---
+++ src/features/extract/utils.py
@@ -0,0 +1,86 @@
+def top20_stats(concept_dict, non_concept_dict, layer, return_feats):
+    top_indices = concept_dict[str(layer)]["top_indices"]
+    top_indices_2 = non_concept_dict[str(layer)]["top_indices"]
+    freq = {}
+    for item in top_indices:
+        for index in item:
+            freq[index] = freq.get(index, 0) + 1
+
+    freq2 = {}
+    for item in top_indices_2:
+        for index in item:
+            freq2[index] = freq2.get(index, 0) + 1
+
+    top_concept, top_non_concept = (
+        sorted(freq, key=freq.get, reverse=True)[:20],
+        sorted(freq2, key=freq2.get, reverse=True)[:20],
+    )
+    to_print = ""
+    to_print +="\nTop 20 feature comparision and respective frequencies left: concept, right: non concept\n\n"
+    for idx1, idx2 in zip(top_concept, top_non_concept):
+        to_print+=f"{int(idx1)}: {freq[idx1]} \t| {int(idx2)}: {freq2[idx2]}\n"
+
+    to_print+="\nfeatures in concept top 20 that are not in non concept non20:\n"
+    feats = []
+    for item in top_concept:
+        if item not in top_non_concept:
+            to_print+=f"{item}, {freq[item]}\n"
+            feats.append(int(item))
+    if not return_feats:
+        return to_print
+    else:
+        return feats
+
+
+def full_feature_set(concept_dict, non_concept_dict, layer):
+    top_indices = concept_dict[str(layer)]["top_indices"]
+    top_indices_2 = non_concept_dict[str(layer)]["top_indices"]
+    freq = {}
+    for item in top_indices:
+        for index in item:
+            freq[index] = freq.get(index, 0) + 1
+
+    freq2 = {}
+    for item in top_indices_2:
+        for index in item:
+            freq2[index] = freq2.get(index, 0) + 1
+    return set(freq.keys()) | set(freq2.keys())
+
+
+def calculate_precision(feat, concept_dict, non_concept_dict, layer):
+    concept_sum, non_concept_sum = 0, 0
+    for item, vals in zip(
+        concept_dict[str(layer)]["top_indices"], concept_dict[str(layer)]["top_values"]
+    ):
+        try:
+            idx = item.index(feat)
+            concept_sum += vals[idx]
+            non_concept_sum += vals[idx]
+        except ValueError:
+            pass
+
+    for item, vals in zip(
+        non_concept_dict[str(layer)]["top_indices"],
+        non_concept_dict[str(layer)]["top_values"],
+    ):
+        try:
+            idx = item.index(feat)
+            non_concept_sum += vals[idx]
+        except ValueError:
+            pass
+    return concept_sum / non_concept_sum
+
+def freq_dicts(concept_dict, non_concept_dict, layer):
+    top_indices = concept_dict[str(layer)]["top_indices"]
+    top_indices_2 = non_concept_dict[str(layer)]["top_indices"]
+    freq = {}
+    for item in top_indices:
+        for index in item:
+            freq[index] = freq.get(index,0)+1
+
+    freq2 = {}
+    for item in top_indices_2:
+        for index in item:
+            freq2[index] = freq2.get(index,0)+1
+    return freq,freq2
+
