c484be92b94417345f71a4a37b479b204a0a907a
duck <duck@duckcom>
2026-05-15 22:36:42 +0000

    new multimodal experiments

--- src/activation_extraction/extract_multimodal.sh
+++ src/activation_extraction/extract_multimodal.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-CONCEPT="a dataset with diverse colors, with some bias towards red, some traditionally red things like blood perhaps"
+CONCEPT="a dataset with diverse colors, with some bias towards blue, some traditionally blue things like the sky perhaps"
 
 LAYERS=(5 10 15 20 30 35 40 50 59)
 
 # CONCEPT_SAFE="${CONCEPT// /_}"
-CONCEPT_SAFE="red"
+CONCEPT_SAFE="blue"
 
 CONCEPT_ACTIVATIONS="activations/multimodal/${CONCEPT_SAFE}_concept_direct_prompt.json"
 NON_CONCEPT_ACTIVATIONS="activations/multimodal/${CONCEPT_SAFE}_non_concept_direct_prompt.json"
@@ -18,14 +18,14 @@ PATCHES_PATH="/workspace/Dataset-Curation/curated_data/image/red_patches/all_red
 INPUT_NAME="/workspace/Dataset-Curation/curated_data/text/text_dataset/text_concept_red.json"
 CLASSIFIED_NAME="/workspace/Dataset-Curation/curated_data/text/text_dataset_classified/text_concept_red_classified.json"
 
-python src/activation_extraction/extract_image_text_activations.py \
-    --image-dir "$IMAGE_DIRECTORY" \
-    --patches-path "$PATCHES_PATH" \
-    --concept "$CONCEPT" \
-    --input-name "$INPUT_NAME" \
-    --classified-name "$CLASSIFIED_NAME" \
-    --output-path "$CONCEPT_ACTIVATIONS" \
-    --layers "${LAYERS[@]}"
+# python src/activation_extraction/extract_image_text_activations.py \
+#     --image-dir "$IMAGE_DIRECTORY" \
+#     --patches-path "$PATCHES_PATH" \
+#     --concept "$CONCEPT" \
+#     --input-name "$INPUT_NAME" \
+#     --classified-name "$CLASSIFIED_NAME" \
+#     --output-path "$CONCEPT_ACTIVATIONS" \
+#     --layers "${LAYERS[@]}"
 
 python src/activation_extraction/extract_image_text_activations.py \
     --image-dir "$IMAGE_DIRECTORY" \
